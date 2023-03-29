b8b3fba9406b9e3f2374bf0e1684f4696b2433c1
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-03-29 09:06:56 +0700

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,13 +3,9 @@ import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDE-J4D7Nc664dCUmwoF5RVBzmm1GG__qI",
-  authDomain: "chat-box-8c5f3.firebaseapp.com",
-  projectId: "chat-box-8c5f3",
-  storageBucket: "chat-box-8c5f3.appspot.com",
-  messagingSenderId: "461556301585",
-  appId: "1:461556301585:web:e71ea32a92dc4535ac16b0",
-  measurementId: "G-Q9XL1W8J9S",
+  // Config here
+  
+  
 };
 
 const app = initializeApp(firebaseConfig);
